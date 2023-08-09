77658b679a4c35d9c03c25657fa775ab50390a56
4mubarak <mubar1000@gmail.com>
2023-08-08 23:06:16 -0700

    Updates

--- 0x00-python-hello_world/0-run.py
+++ 0x00-python-hello_world/0-run.py
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-# Check that $PYFILE is set with the Python file name
-if [ -z "$PYFILE" ]
-then
-    echo "Please set the PYFILE environment variable with the Python script name" >&2
-    exit 1
-fi
-
-# Run the Python script
-python $PYFILE
+python3 $PYFILE
